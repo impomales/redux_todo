b3d4b7bff1156d1877cb1061ba6a82ade3d68bdf
Isaias <impomales@gmail.com>
2017-03-31 14:31:23 -0400

    add reducer for adding a todo

--- reducers.js
+++ reducers.js
@@ -11,3 +11,18 @@ function visibilityFilter(state = SHOW_ALL, action) {
 			return state;
 	}
 }
+
+function todos(state = [], action) {
+	switch (action.type) {
+		case ADD_TODO:
+			return [
+				...state,
+				{
+					text: action.text,
+					completed: false
+				}
+			]
+		default:
+			return state;
+	}
+}
