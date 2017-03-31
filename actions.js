1fed75dce92c1700b78146531c1d6884603a8d45
Isaias <impomales@gmail.com>
2017-03-31 14:14:50 -0400

    defined actions

---
+++ actions.js
@@ -0,0 +1,28 @@
+/* action types */
+
+export const ADD_TODO = 'ADD_TODO'
+export const TOGGLE_TODO = 'TOGGLE_TODO'
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER'
+
+/* visibility constants */
+
+export const VisibilityFilters = {
+	SHOW_ALL: 'SHOW_ALL',
+	SHOW_COMPLETED: 'SHOW_COMPLETED',
+	SHOW_ACTIVE: 'SHOW_ACTIVE'
+}
+
+// ecmaScript 2015 allows shorthand syntax
+// for older, would need to be {type: 'ADD_TODO', text: text}
+
+export function addTodo(text) {
+	return { type: ADD_TODO, text };
+}
+
+export function toggleTodo(index) {
+	return { type: TOGGLE_TODO, index };
+}
+
+export function setVisibilityFilter(filter) {
+	return { type: SET_VISIBILITY_FILTER, filter }
+}
