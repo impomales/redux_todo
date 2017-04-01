faecce00e2a1774860540ddf396622132a7fb9d6
Isaias <impomales@gmail.com>
2017-04-01 15:33:01 -0400

    create todo list item component

---
+++ source/components/Todo.js
@@ -0,0 +1,17 @@
+import React, { ProcType } from 'react';
+
+const Todo = ({ onClick, completed, text }) => (
+	<li
+		onClick={onClick}
+		style={{textDecoration: completed ? 'line-through' : 'none'}}
+	> {text}
+	</li>
+);
+
+Todo.propTypes = {
+	onClick: PropTypes.func.isRequired,
+	completed: PropTypes.bool.isRequired,
+	text: PropTypes.string.isRequired
+};
+
+export default Todo;
